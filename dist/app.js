9cd68523031a10f0f18652e839be708e7254171a
Dimitar Staykov <guho3abap@gmail.com>
2019-10-19 23:29:05 +0100

    initial commit

--- dist/app.js
+++ dist/app.js
@@ -5,33 +5,35 @@ const images = document.querySelectorAll(".img");
 const BtnPlay = document.getElementById("btn-Play");
 const BtnStop = document.getElementById("btn-Stop");
 const displayBoard = document.querySelector(".display");
-
 const leaves = ["orange", "red", "yellow", "brown", "gold", "burgund", "bronze", "bordo"];
 
 let music = new Audio();
+let isPlaying = false;
 let duration;
 let countdown;
 
-//get random Number ========================
+//========= get random Number ========================
 function randomNumber(limit){
   return Math.floor((Math.random() * limit));
 }
 
-//get random range number
+//========= get random range number =====================
 function randomTime(min , max){
   return Math.round((Math.random()* (max - min)) + min);
 }
 
-//create Leaf  ======================================
+//=========== create Leaf  ============================
 function createLeaf(){
 
 let leafColor = leaves[randomNumber(leaves.length)];
 const div = document.createElement("div");
-
+const fallTime = randomTime(20, 30);
+const animation = randomNumber(5) + 1
   div.setAttribute("class", "leaf");
   div.style.left = `${randomNumber(100)}%`;
   div.style.backgroundImage = `url('images/${leafColor}.png')`;
-  div.style.animation = `leaf-fall-${randomNumber(3) + 1} ${randomTime(20, 30)}s ease-in forwards`;
+  div.style.animation = `leaf-fall-${animation} ${fallTime}s ease-in forwards`;
+  //div.textContent = `${animation} ${fallTime}`;
   body.appendChild(div);
 
 }
@@ -39,20 +41,24 @@ const div = document.createElement("div");
 //play the music and start the leaf falling ==============
 function play(){
 
-const time = Math.round(Number(music.duration)) * 1000;
-const seconds = Math.floor((time/1000));
-
+//const time = Math.round(Number(music.duration)) * 1000;
+const seconds = Math.round(music.duration);
+console.log(seconds);
   timer(seconds);
   BtnPlay.style.display = "none";
   displayBoard.style.opacity = "1";
   music.play();
   createLeaf();
-  duration = setInterval(createLeaf, 3000);
 
-     setTimeout(()=> {
+    duration = setInterval(()=>{
+
+      createLeaf();
+
+      if(!isPlaying){
         clearInterval(duration);
-        setTimeout(()=> stop() ,30000)
-       }, time)
+        setTimeout(()=> stop() ,25000)
+        }
+      },3000);
 }
 
 // stop the animation ==============
@@ -80,7 +86,9 @@ const background = img.querySelector("img").getAttribute("src");
   body.style.backgroundImage = `url('${background}')`;
   navBar.style.opacity = "0";
 
-  setTimeout(()=> navBar.style.display = "none", 2000)
+  setTimeout(()=> {
+    navBar.style.display = "none"
+    BtnPlay.style.display = "block"}, 1500)
    })
 )
 
@@ -92,9 +100,11 @@ const then = now + seconds * 1000;
 displayTimeLeft(seconds)
 
 countdown = setInterval(()=>{
+
 const secondsLeft = Math.round((then - Date.now()) /1000);
    if(secondsLeft < 0){
      clearInterval(countdown);
+     isPlaying = false;
      return;
       }
    displayTimeLeft(secondsLeft)
@@ -104,7 +114,7 @@ const secondsLeft = Math.round((then - Date.now()) /1000);
 
 // display the remaining time ======================
 function displayTimeLeft(seconds){
-
+isPlaying = true;
 const minutes = Math.floor(seconds/60);
 const remainingSeconds = seconds % 60;
 const display = `${minutes}:${remainingSeconds < 10 ? 0 : ''}${remainingSeconds}`;
